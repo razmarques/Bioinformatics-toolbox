aee1c77b29ca7b0cf9fb33cdce88d7b9207027e7
Rodolfo Marques <43847610+rodmarq@users.noreply.github.com>
2019-07-03 11:45:02 +0100

    added functions to utilities module
    Signed-off-by: Rodolfo Marques <43847610+rodmarq@users.noreply.github.com>

--- utilities.py
+++ utilities.py
@@ -3,8 +3,24 @@ This module contains several functions for basic computations with nucleotides
 """
 
 
+def compute_dna_complement(dna_seq):
+    seq_c = ''
+    for nuc in dna_seq:
+        if nuc == 'A':
+            seq_c += 'T'
+        elif nuc == 'T':
+            seq_c += 'A'
+        elif nuc == 'C':
+            seq_c += 'G'
+        elif nuc == 'G':
+            seq_c += 'C'
+        else:
+            raise ValueError("Unrecognized nucleotide")
+    return seq_c
+
+
 def count_nucleotides(seq):
-    nuc_counter = {'A':0, 'C':0, 'G':0, 'T':0, 'U':0}
+    nuc_counter = {'A': 0, 'C': 0, 'G': 0, 'T': 0, 'U': 0}
     for nuc in seq:
         if nuc == 'A':
             nuc_counter['A'] += 1
@@ -19,6 +35,11 @@ def count_nucleotides(seq):
         else:
             raise ValueError("Unrecognized nucleotide")
     return nuc_counter
+
+
+def compute_reverse_complement(dnaseq):
+    dna_comp = compute_dna_complement(dnaseq)
+    return dna_comp[::-1] # returns the reverse of dna_comp string
 
 
 def thymine_to_uracil(dnaseq):
