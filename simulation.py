e7aa167cbb58e0e939775dff4b2206750779d835
rodmarq <43847610+rodmarq@users.noreply.github.com>
2019-11-04 16:12:54 +0000

    Addeed docstring documentation
    Signed-off-by: Rodolfo Marques <43847610+rodmarq@users.noreply.github.com>

--- simulation.py
+++ simulation.py
@@ -6,6 +6,15 @@ import random
 
 
 def generate_random_sequence(sequence_size, type):
+    """
+    Generates a random nucleotide sequence of a given type (DNA or RNA)
+    Arguments:
+         sequence_size (int) -- number of base pairs of the nucleotide sequence
+         type (string) -- defines the type of the nucleotide sequence: "dna" or "rna"
+
+     Returns:
+         sequence (string) -- sequence of nucleotides
+    """
 
     sequence = ""
     if type == "dna":
